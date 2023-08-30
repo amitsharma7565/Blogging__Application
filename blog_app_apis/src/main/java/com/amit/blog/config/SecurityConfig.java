5da6a74e594dad19f2fa53f7d7dd74f22fc441ae
Amit Sharma <amitsharma7565@gmail.com>
2023-08-30 07:51:44 +0530

    Swagger documentation in progress

--- blog_app_apis/src/main/java/com/amit/blog/config/SecurityConfig.java
+++ blog_app_apis/src/main/java/com/amit/blog/config/SecurityConfig.java
@@ -11,22 +11,28 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.amit.blog.security.CustomUserDetailService;
 import com.amit.blog.security.JwtAuthenticationEntryPoint;
 import com.amit.blog.security.JwtAuthenticationFilter;
 
-
-
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled=true)
-public class SecurityConfig {
+@EnableWebMvc 
+public class SecurityConfig  {
+	//extends WebSecurityConfiguration
+	//define array and in this array store all url that i want to do public
+	public static final String[] PUBLIC_URLS= {
+			"/api/v1/auth/**",
+	};
 	
 	@Autowired
 	private CustomUserDetailService customUserDetailService;
@@ -42,8 +48,8 @@ public class SecurityConfig {
       .disable()
       .authorizeHttpRequests()
 //      login api public if i want to public any other then pass here and then that url is public 
-      // and here this two ** means it allow that 2 methos that is present in AuthController
-      .requestMatchers("/api/v1/auth/**").permitAll()
+      // and here this two ** means it allow that 2 methods that is present in AuthController
+      .requestMatchers(PUBLIC_URLS).permitAll()
       // this 46 line is for all person access the get method without login access 
       .requestMatchers(HttpMethod.GET).permitAll()
       .anyRequest()
